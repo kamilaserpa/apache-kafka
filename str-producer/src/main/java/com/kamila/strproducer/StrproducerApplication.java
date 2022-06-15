c1c22b271adb9e7422a927201612eb4f6174203e
Kamila Serpa <kamila.serpa@softplan.com.br>
2022-06-14 21:25:23 -0300

    Iniciando str-producer

---
+++ str-producer/src/main/java/com/kamila/strproducer/StrproducerApplication.java
@@ -0,0 +1,13 @@
+package com.kamila.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrproducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.kamila.strproducer.StrproducerApplication.class, args);
+    }
+
+}
